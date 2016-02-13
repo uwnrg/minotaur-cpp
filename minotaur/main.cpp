cbbf4f2feee1aaf3677ba0c2b9964e0ca38da541
Sadman Kazi <sadmansazidk@gmail.com>
2016-02-13 13:55:47 -0500

    Implement unlinked GUI in C++

---
+++ minotaur/main.cpp
@@ -0,0 +1,19 @@
+#include "mainwindow.h"
+#include "logger.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    MainWindow *w = new MainWindow();
+
+    Logger *logger = new Logger();
+    w->show();
+
+    //initialize all the controllers
+    std::string message = "Controllers initialized";
+    //bool shit = logger.Log(message);
+
+    //delete logger;
+    return app.exec();
+}
