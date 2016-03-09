4e21d7a849ab62042bdfaf814ddb4a13bb338815
Sadman Kazi <sadmansazidk@gmail.com>
2016-03-08 21:19:11 -0500

    Add serial port setup and zaber helper functions

--- code/controller/actuator.cpp
+++ code/controller/actuator.cpp
@@ -6,16 +6,54 @@ Actuator::Actuator(const QString& serial_port, QextSerialPort::QueryMode mode, Q
     //on successful connection
     if (m_serial_port->lastError() == 0) {
         Logger::Log(serial_port.toStdString() + " successfully opened!", Logger::INFO);
+        // For Zaber, settings must be: 9600 baud, no hand shaking, 8 data bits, no parity, one stop bit.
         m_serial_port->flush();
         m_serial_port->setBaudRate(BaudRateType(BAUD_RATE));
+        m_serial_port->setFlowControl(FLOW_XONXOFF);
+        m_serial_port->setDataBits(DATA_8);
+        m_serial_port->setParity(PAR_NONE);
+        m_serial_port->setStopBits(STOP_1);
     }
     else {
         Logger::Log("ERROR: " + serial_port.toStdString() + " could not be opened! " + m_serial_port->errorString().toStdString(), Logger::ERROR);
     }
 }
 
+char* const Actuator::ConvertDataToBytes(long int data) {
+	if (data < 0) {
+		data = IntPow(BYTE_RANGE, 4) + data;
+	}
+
+	char result[DATA_SIZE];
+	
+	for (int i = DATA_SIZE - 1; i >= 0; ++i) {
+		int temp = IntPow(BYTE_RANGE, i);
+		result[i] = data / temp;
+		data = data - temp;
+	}
+
+	return result;
+}
+
+void Actuator::SetDeviceNumber() {
+	char instr[6];
+}
+
+int const Actuator::IntPow(int x, int p) {
+	if (p == 0) return 1;
+	if (p == 1) return x;
+
+	int tmp = IntPow(x, p / 2);
+	if (p % 2 == 0) return tmp * tmp;
+	else return x * tmp * tmp;
+}
+
 Actuator::~Actuator() {
     delete m_serial_port;
 }
 
-void Actuator::Move(Controller::Dir) {}
+void Actuator::Move(Controller::Dir) {
+
+}
+
+//TODO: Add static method for getting current configuration of a given port
--- code/controller/actuator.h
+++ code/controller/actuator.h
@@ -6,6 +6,9 @@
 
 
 #define BAUD_RATE 9600
+#define INSTR_SIZE 6
+#define DATA_SIZE 4
+#define BYTE_RANGE 256
 
 
 class QextSerialPort;
@@ -15,15 +18,29 @@ class Actuator : public Controller
 public:
     Actuator(const QString& serial_port, QextSerialPort::QueryMode mode = QextSerialPort::EventDriven, QObject * parent = 0);
 
-    void SetComPort();
+    void SetSerPort(const QString& serial_port);
+    QString* GetAvailablePorts() const;
     void Move(Controller::Dir);
     ~Actuator();
 
 private:
+    enum CMD {
+        HOME = 1,
+        RENUMBER = 2,
+        ABS_MOVE = 20,
+        REL_MOVE = 21,
+        CONST_SPEED_MOVE = 22,
+        STOP_MOVE = 23,
+        SET_CURRENT_POS = 45,
+        FIRM_VER = 51
+    };
     QextSerialPort* m_serial_port;
+    unsigned char m_device_number, m_data_buffer;
 
     //helper functions
-
+	char* const ConvertDataToBytes(long int data);
+	void SetDeviceNumber();
+	int const IntPow(int x, int p);
 };
 
 #endif // ACTUATOR_H
