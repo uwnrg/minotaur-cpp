0a09d2109163160c5543e081355a5560d9a6cc94
Mogball <jeffniu22@gmail.com>
2016-11-10 23:03:45 -0500

    Made changes to adhere to guidelines in CONTRIBUTING.md
    Move member field initialization to initializer list in actuator.cpp
    
    Added protected constructor to controller.h

--- code/controller/actuator.cpp
+++ code/controller/actuator.cpp
@@ -4,10 +4,10 @@
 #include "actuator.h"
 #include "../utility/logger.h"
 
-Actuator::Actuator(const QString& serial_port, const PortSettings& settings, QextSerialPort::QueryMode mode) {
-    m_serial_port = new QextSerialPort(serial_port, settings, mode);
-	m_invert_x = 1;
-	m_invert_y = 1;
+Actuator::Actuator(const QString& serial_port, 
+	const PortSettings& settings, QextSerialPort::QueryMode mode) :
+	Controller(1, 1), m_serial_port(new QextSerialPort(serial_port, settings, mode)) {
+
     //on successful connection
     if (m_serial_port->lastError() == 0) {
         Logger::log(serial_port.toStdString() + " successfully opened!", Logger::INFO);
@@ -20,14 +20,11 @@ Actuator::Actuator(const QString& serial_port, const PortSettings& settings, Qex
     }
 }
 
-Actuator::Actuator(const Actuator& other) {
-	m_x_device = other.m_x_device;
-	m_y_device = other.m_y_device;
-	m_invert_x = other.m_invert_x;
-	m_invert_y = other.m_invert_y;
-
-	m_serial_port = other.m_serial_port;
-}
+// Copy constructor
+Actuator::Actuator(const Actuator& other) :
+	Controller(other.m_invert_x, other.m_invert_y),
+	m_x_device(other.m_x_device), m_y_device(other.m_y_device),
+	m_serial_port(other.m_serial_port) {}
 
 char* const Actuator::convertDataToBytes(long int data) {
 	if (data < 0) {
