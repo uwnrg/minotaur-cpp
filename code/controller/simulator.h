53efed48b4bc83d34ce3ce18cbcd5c239c3f0cd7
Jeff Niu <jeffniu22@gmail.com>
2017-05-16 15:57:54 -0400

    added definitions for simulator.cpp

--- code/controller/simulator.h
+++ code/controller/simulator.h
@@ -7,7 +7,8 @@
 
 class Simulator : public Controller {
 public:
-    void move(Dir dir, int time = STEP_TIME);
+	Simulator(int t_invert_x, int t_invert_y);
+	void move(Dir dir, int time = STEP_TIME);
     void move(Vector2i dir, int time = STEP_TIME);
 };
 
