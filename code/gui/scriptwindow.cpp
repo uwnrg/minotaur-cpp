98888991cda21f5c4e03af404e3a8a80f615f2d1
Sadman Kazi <sadman@sadmansk.com>
2017-06-22 11:14:36 -0700

    Update scriptwindow.cpp

--- code/gui/scriptwindow.cpp
+++ code/gui/scriptwindow.cpp
@@ -1,7 +1,7 @@
 #include "scriptwindow.h"
 #include "ui_scriptwindow.h"
 
-ScriptWindow::ScriptWindow(std::shared_ptr<ScriptEngine>& script_engine, QWidget *parent) :
+ScriptWindow::ScriptWindow(std::shared_ptr<ScriptEngine>& script_engine, QWidget* parent) :
     QDialog(parent),
     ui(new Ui::ScriptWindow),
     m_script_engine(script_engine)
