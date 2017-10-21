6563003cfea7b123a8af85c956ca98ff0f423145
Jeff Niu <jeffniu22@gmail.com>
2017-10-21 16:08:25 -0700

    Modified travis.yml

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Installing Git
-sudo apt-get update
-sudo apt-get install git -y
+sudo apt-get update -qq
+sudo apt-get install git -y -qq
 
 # Installing Qt
 wget http://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-5.7.0.run
 chmod +x qt-opensource-linux-x64-5.7.0.run
-sudo apt-get install build-essential libfontconfig1 mesa-common-dev libglu1-mesa-dev -y
+sudo apt-get install build-essential libfontconfig1 mesa-common-dev libglu1-mesa-dev -y -qq
 
 # Grab Qt installer script
 git clone https://github.com/benlau/qtci.git
@@ -18,10 +18,10 @@ extract-qt-installer qt-opensource-linux-x64-5.7.0.run /home/vagrant/Qt5.7.0
 export PATH=/home/vagrant/Qt5.7.0/5.7/gcc_64/bin:$PATH
 
 # Installing Python
-sudo apt-get install python-software-properties -y
+sudo apt-get install python-software-properties -y -qq
 sudo add-apt-repository ppa:fkrull/deadsnakes -y
-sudo apt-get update
-sudo apt-get install python3.4 python3.4-dev -y
+sudo apt-get update -qq
+sudo apt-get install python3.4 python3.4-dev -y -qq
 python3.4 --version
 
 # Install cmake
@@ -36,15 +36,15 @@ cmake --version
 
 # Installing gcc
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt-get update
-sudo apt-get install gcc-4.8 g++-4.8 -y
+sudo apt-get update -qq
+sudo apt-get install gcc-4.8 g++-4.8 -y -qq
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 20
 sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
 gcc --version
 g++ --version
 
 # Additional dependencies
-sudo apt-get install libudev-dev libxi6 libsm6 libxrender1 libegl1-mesa -y
+sudo apt-get install libudev-dev libxi6 libsm6 libxrender1 libegl1-mesa -y -qq
 
 # Build and run
 cmake CMakeLists.txt
---
+++ setup_fast.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+sudo apt-get update -qq
+sudo apt-get install python-software-properties -y -qq
+
+sudo add-apt-repository ppa:fkrull/deadsnakes -y
+sudo add-apt-repository ppa:george-edison55/precise-backports -y
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo add-apt-repository ppa:canonical-qt5-edgers/ubuntu1204-qt5 -y
+
+sudo apt-get update -qq
+
+sudo apt-get install qtdeclarative5-dev -y -qq
+sudo apt-get install python3.4 python3.4-dev -y -qq
+sudo apt-get install cmake -y -qq
+sudo apt-get install g++-4.8 -y -qq
+
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 20
+
+python3.4 --version
+cmake --version
+g++ --version
+
+sudo apt-get install libudev-dev libxi6 libsm6 libxrender1 libegl1-mesa -y -qq
+
+cmake CMakeLists.txt
+make
+./minotaur-cpp
