0993f41201b5c34360a075d31ca0754a807aa63d
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2018-02-21 13:59:51 -0500

    Fixed CMake error

--- CMakeFiles/minotaur-cpp_autogen.dir/AutogenInfo.cmake
+++ CMakeFiles/minotaur-cpp_autogen.dir/AutogenInfo.cmake
@@ -1,3 +1,5 @@
+# Meta
+set(AM_MULTI_CONFIG "SINGLE")
 # Directories and files
 set(AM_CMAKE_BINARY_DIR "/Users/Beini/minotaur-cpp/")
 set(AM_CMAKE_SOURCE_DIR "/Users/Beini/minotaur-cpp/")
@@ -9,25 +11,29 @@ set(AM_SOURCES "/Users/Beini/minotaur-cpp/code/controller/controller.cpp;/Users/
 set(AM_HEADERS "/Users/Beini/minotaur-cpp/code/controller/controller.h;/Users/Beini/minotaur-cpp/code/controller/simulator.h;/Users/Beini/minotaur-cpp/code/controller/solenoid.h;/Users/Beini/minotaur-cpp/code/graphics/robotgraphicsitem.h;/Users/Beini/minotaur-cpp/code/graphics/simulatorscene.h;/Users/Beini/minotaur-cpp/code/gui/actionabout.h;/Users/Beini/minotaur-cpp/code/gui/camera.h;/Users/Beini/minotaur-cpp/code/gui/mainwindow.h;/Users/Beini/minotaur-cpp/code/gui/scripteditor.h;/Users/Beini/minotaur-cpp/code/gui/scriptwindow.h;/Users/Beini/minotaur-cpp/code/gui/simulatorwindow.h;/Users/Beini/minotaur-cpp/code/interpreter/codeeditor.h;/Users/Beini/minotaur-cpp/code/interpreter/embeddedcontroller.h;/Users/Beini/minotaur-cpp/code/interpreter/highlighter.h;/Users/Beini/minotaur-cpp/code/interpreter/interpretertextedit.h;/Users/Beini/minotaur-cpp/code/interpreter/python.h;/Users/Beini/minotaur-cpp/code/interpreter/pythonengine.h;/Users/Beini/minotaur-cpp/code/interpreter/resultstextdisplay.h;/Users/Beini/minotaur-cpp/code/simulator/arrow.h;/Users/Beini/minotaur-cpp/code/simulator/drawable.h;/Users/Beini/minotaur-cpp/code/simulator/renderscene.h;/Users/Beini/minotaur-cpp/code/simulator/renderscenebase.h;/Users/Beini/minotaur-cpp/code/simulator/sam.h;/Users/Beini/minotaur-cpp/code/simulator/simulatorSolenoid.h;/Users/Beini/minotaur-cpp/code/simulator/statsdisplay.h;/Users/Beini/minotaur-cpp/code/utility/clock_time.h;/Users/Beini/minotaur-cpp/code/utility/logger.h;/Users/Beini/minotaur-cpp/code/utility/vector2i.h;/Users/Beini/minotaur-cpp/code/video/modify.h;/Users/Beini/minotaur-cpp/code/video/recorder.h;/Users/Beini/minotaur-cpp/code/video/shapedetect.h;/Users/Beini/minotaur-cpp/code/video/squares.h;/Users/Beini/minotaur-cpp/code/video/tracker.h")
 # Qt environment
 set(AM_QT_VERSION_MAJOR "5")
+set(AM_QT_VERSION_MINOR "7")
 set(AM_QT_MOC_EXECUTABLE "/usr/local/opt/qt@5.7/bin/moc")
 set(AM_QT_UIC_EXECUTABLE "/usr/local/opt/qt@5.7/bin/uic")
 set(AM_QT_RCC_EXECUTABLE )
 # MOC settings
-set(AM_MOC_SKIP "")
+set(AM_MOC_SKIP "/Users/Beini/minotaur-cpp/minotaur-cpp_autogen/mocs_compilation.cpp")
 set(AM_MOC_DEFINITIONS "PYTHON_SCRIPT_DIR=\"python_scripts\";QT_CORE_LIB;QT_GUI_LIB;QT_MULTIMEDIAWIDGETS_LIB;QT_MULTIMEDIA_LIB;QT_NETWORK_LIB;QT_WIDGETS_LIB")
 set(AM_MOC_INCLUDES "/Users/Beini/minotaur-cpp;/Users/Beini/minotaur-cpp/minotaur-cpp_autogen/include;/usr/local/opt/qt@5.7/lib/QtWidgets.framework;/usr/local/opt/qt@5.7/lib/QtWidgets.framework/Headers;/usr/local/opt/qt@5.7/lib/QtGui.framework;/usr/local/opt/qt@5.7/lib/QtGui.framework/Headers;/usr/local/opt/qt@5.7/lib/QtCore.framework;/usr/local/opt/qt@5.7/lib/QtCore.framework/Headers;/usr/local/opt/qt@5.7/./mkspecs/macx-clang;/System/Library/Frameworks/OpenGL.framework/Headers;/usr/local/Frameworks/Python.framework/Versions/3.6/include/python3.6m;/Users/Beini/minotaur-cpp/third-party;/usr/local/include;/usr/local/include/opencv;/usr/local/opt/qt@5.7/lib/QtMultimedia.framework;/usr/local/opt/qt@5.7/lib/QtMultimedia.framework/Headers;/usr/local/opt/qt@5.7/lib/QtNetwork.framework;/usr/local/opt/qt@5.7/lib/QtNetwork.framework/Headers;/usr/local/opt/qt@5.7/lib/QtMultimediaWidgets.framework;/usr/local/opt/qt@5.7/lib/QtMultimediaWidgets.framework/Headers")
 set(AM_MOC_OPTIONS "")
 set(AM_MOC_RELAXED_MODE "FALSE")
+set(AM_MOC_MACRO_NAMES "Q_OBJECT;Q_GADGET;Q_NAMESPACE")
 set(AM_MOC_DEPEND_FILTERS "")
 set(AM_MOC_PREDEFS_CMD )
 # UIC settings
-set(AM_UIC_SKIP "")
+set(AM_UIC_SKIP "/Users/Beini/minotaur-cpp/minotaur-cpp_autogen/mocs_compilation.cpp")
 set(AM_UIC_TARGET_OPTIONS "")
 set(AM_UIC_OPTIONS_FILES "")
 set(AM_UIC_OPTIONS_OPTIONS "")
 set(AM_UIC_SEARCH_PATHS "")
 # RCC settings
-set(AM_RCC_SOURCES  )
+set(AM_RCC_SOURCES )
+set(AM_RCC_BUILDS )
+set(AM_RCC_OPTIONS )
 set(AM_RCC_INPUTS )
-set(AM_RCC_OPTIONS_FILES )
-set(AM_RCC_OPTIONS_OPTIONS )
+# Configurations options
+set(AM_CONFIG_SUFFIX_Debug "_Debug")
