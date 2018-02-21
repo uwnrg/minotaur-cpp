84ecfaaa924063915e58d7f1941ec811c614be39
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2018-02-21 12:45:43 -0500

    Added video recording feature

---
+++ code/video/recorder.h
@@ -0,0 +1,31 @@
+#ifndef MINOTAUR_CPP_RECORDER_H
+#define MINOTAUR_CPP_RECORDER_H
+
+#include <opencv2/opencv.hpp>
+#include <QObject>
+
+#include "modify.h"
+
+class Recorder : public QObject {
+Q_OBJECT
+
+public:
+    Recorder(const std::string &file_name, int four_cc, double frame_rate, cv::Size size, bool is_color = true);
+
+    void modify(cv::Mat *img);
+
+    void finish();
+
+    Q_SLOT void image_received(cv::Mat &img);
+
+    Q_SLOT void stop_recording();
+
+    bool is_recording();
+
+private:
+    cv::VideoWriter m_video_writer;
+
+    bool m_is_recording;
+};
+
+#endif //MINOTAUR_CPP_RECORDER_H
