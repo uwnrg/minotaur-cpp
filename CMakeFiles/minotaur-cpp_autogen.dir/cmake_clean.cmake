710b24048f3af9dcd13b3f52af04e95c3b0dda37
Ding Bei Ni Fang <dbnfang@uwaterloo.ca>
2018-02-21 13:43:49 -0500

    Fixed compilation errors

---
+++ CMakeFiles/minotaur-cpp_autogen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "minotaur-cpp_autogen"
+  "CMakeFiles/minotaur-cpp_autogen.dir/AutogenOldSettings.cmake"
+  "CMakeFiles/minotaur-cpp_autogen"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/minotaur-cpp_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
