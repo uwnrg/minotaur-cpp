f49e7d4cd11827d6a330b35af6f3c9b30467a08f
UWNRG Controls <uwnrg.controls@gmail.com>
2018-02-26 15:03:51 -0500

    Queueing

--- code/controller/solenoid.cpp
+++ code/controller/solenoid.cpp
@@ -1,31 +1,56 @@
 #include "solenoid.h"
-#include <ostream>
 
-Solenoid::Solenoid(const QString& serial_port): Controller(1, 1) {
+MovementExecutor::MovementExecutor(Solenoid *solenoid)
+    : m_solenoid(solenoid) {}
+
+MovementExecutor::MovementExecutor(Solenoid *solenoid, const std::string &port)
+    : m_solenoid(solenoid) {
+    m_stream.open(port);
+    if (!m_stream.is_open()) {
+        Logger::log("Failed to connect to serial port: " + port, Logger::FATAL);
+        m_stream.close();
+    }
+}
+
+void MovementExecutor::queue_movement(Vector2i dir) {
+    m_buffer.push(dir);
+}
+
+void MovementExecutor::execute_movement() {
+    if (m_buffer.empty()) {
+        return;
+    }
+    m_mutex.lock();
+    Vector2i next_movement = m_buffer.front();
+    m_buffer.pop();
+    m_mutex.unlock();
+}
+
+Solenoid::Solenoid(const QString &serial_port) : Controller(1, 1) {
     // TODO: change actuator setup to be used for general serial set up
-    if ( serial_port.isEmpty() ) {
+    if (serial_port.isEmpty()) {
         // TODO: provide error if this does not succeed
-        stream.open("/dev/ttyACM1");        // manually changed to match port indicated in arduino IDE
+        m_stream.open("/dev/ttyACM0");        // manually changed to match port indicated in arduino IDE
     } else {
-        stream.open(serial_port.toStdString());
+        m_stream.open(serial_port.toStdString());
     }
 }
 
 Solenoid::~Solenoid() {
-    stream.close();
+    m_stream.close();
 }
 
 void Solenoid::move(Vector2i dir, int timer) {
     // TODO: user timer for movement control
     bool success = true;
 #ifndef NDEBUG
-	Logger::log("Moving Solenoid controller", Logger::DEBUG);
+    Logger::log("Moving Solenoid controller", Logger::DEBUG);
     Logger::log("Attempting move (" + std::to_string(dir.x_comp) + ", " + std::to_string(dir.y_comp) + ")",
                 Logger::DEBUG);
 #endif
     try {
-        stream << vectorToBinary(dir);
-        stream.flush();
+        m_stream << vectorToBinary(dir);
+        m_stream.flush();
     } catch (...) {
         success = false;
 #ifndef NDEBUG
@@ -40,35 +65,29 @@ void Solenoid::move(Vector2i dir, int timer) {
     }
 }
 
-int Solenoid::vectorToBinary(Vector2i dir) {
+int MovementExecutor::vector_to_bin(Vector2i dir) {
     int direction = 0b0;
 
-    switch(dir.x_comp) {
+    switch (dir.x_comp) {
         case 1:
             direction = direction | Direction::UP;
             break;
         case -1:
             direction = direction | Direction::DOWN;
             break;
-        case 0:
-            // DO NOTHING
-            break;
         default:
-            Logger::log("Invalid direction x component", Logger::FATAL);
+            break;
     }
 
-    switch(dir.y_comp) {
+    switch (dir.y_comp) {
         case 1:
             direction = direction | Direction::RIGHT;
             break;
         case -1:
             direction = direction | Direction::LEFT;
             break;
-        case 0:
-            // DO NOTHING
-            break;
         default:
-            Logger::log("Invalid direction y component", Logger::FATAL);
+            break;
     }
 
     return direction;
--- code/controller/solenoid.h
+++ code/controller/solenoid.h
@@ -2,17 +2,59 @@
 #define SOLENOID_H
 
 #include "controller.h"
-#include <fstream>
 
-class Solenoid : public Controller {
+#include <fstream>
+#include <queue>
+
+#include <QObject>
+#include <QSerialPort>
+#include <QMutex>
+
+class Solenoid : public Controller, public QObject {
+Q_OBJECT
+
 public:
-    Solenoid(const QString& serial_port = "");
+    Solenoid(const QString &serial_port = "");
+
     ~Solenoid();
+
     void move(Vector2i dir, int timer);
+
 private:
-    enum Direction { UP = 0b1000, RIGHT = 0b0100, DOWN = 0b0010, LEFT = 0b0001 };
-    std::ofstream stream;
-    int vectorToBinary(Vector2i dir);
+
+    std::ofstream m_stream;
+};
+
+class MovementExecutor : public QObject {
+Q_OBJECT
+
+public:
+    static int vector_to_bin(Vector2i dir);
+
+    explicit MovementExecutor(Solenoid *solenoid);
+
+    MovementExecutor(Solenoid *solenoid, const std::string &port);
+
+    Q_SLOT void queue_movement(Vector2i vec);
+
+    Q_SLOT void wake();
+
+    Q_SLOT void sleep();
+
+    Q_SLOT void execute_movement();
+
+private:
+    enum Direction {
+        UP = 0b1000,
+        RIGHT = 0b0100,
+        DOWN = 0b0010,
+        LEFT = 0b0001
+    };
+
+    QMutex m_mutex;
+    QSerialPort m_serial;
+    Solenoid *m_solenoid;
+    std::queue<Vector2i> m_buffer;
 };
 
 #endif // SOLENOID_H
